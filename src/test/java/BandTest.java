9174a8bbfe2b71cbe118e68eec1dd6d1be5f15c3
Brad London <london.brad@gmail.com>
2016-03-04 08:04:46 -0800

    recognize if two bands are named the same

--- src/test/java/BandTest.java
+++ src/test/java/BandTest.java
@@ -12,4 +12,11 @@ public class BandTest {
   public void all_emptyAtFirst() {
     assertEquals(Band.all().size(), 0);
   }
+
+  @Test
+  public void equals_returnsTrueIfNamesAretheSame() {
+    Band firstBand = new Band("Go Go Boogie");
+    Band secondBand = new Band("Go Go Boogie");
+    assertTrue(firstBand.equals(secondBand));
+  }
 }
